e197ae8f77b01420a30d89202bf60184d5ca7ca4
Piyush Pandey <piyush.pandey@tiket.com>
2024-04-02 23:58:01 +0530

    day 1 - adding and organizing the files

--- TextIt/Controllers/ConversationsViewController.swift
+++ TextIt/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  TextIt
 //
 //  Created by Piyush Pandey on 02/04/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
